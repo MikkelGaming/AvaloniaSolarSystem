65cac27606a0f8125afe443e4ff98dd09a3f9fb8
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2025-09-19 18:43:13 +0200

    More fair comparison

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -11,6 +11,7 @@ public partial class MainWindow : Window
 {
     Vector2D center;
     List<Planet> planets = [];
+    DispatcherTimer timer;
 
     public MainWindow()
     {
@@ -20,91 +21,61 @@ public partial class MainWindow : Window
 
         center = new();
 
-        DispatcherTimer timer = new DispatcherTimer();
-        timer.Interval = TimeSpan.FromMilliseconds(1);
+        timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromMilliseconds(10);
 
 
         Planet sun = new(140, 140, 0, new Vector2D(center.X, center.Y), Brushes.Yellow, 0);
         mainCanvas.Children.Add(sun.Shape);
         planets.Add(sun);
+        timer.Tick += sun.Move;
 
-        Planet earth = new(
-            30,
-            30,
-            0.04,
-            new Vector2D(center.X, center.Y),
-            Brushes.Blue,
-            200
+
+        CreatePlanetWithMoons(
+            new Planet(30, 30, 0.04, new Vector2D(center.X, center.Y), Brushes.Green, 200),
+            new Moon(5, 5, 0.06, null, Brushes.LightGray, 50),
+            0,
+            1,
+            true,
+            10
         );
-        mainCanvas.Children.Add(earth.Shape);
-        planets.Add(earth);
 
-        Moon earthMoon = new(
-            5,
-            5,
-            0.06,
-            earth,
-            Brushes.LightGray,
+
+        CreatePlanetWithMoons(
+            new Planet(20, 20, 0.02, new Vector2D(center.X, center.Y), Brushes.Green, 300),
+            new Moon(2, 2, 0.01, null, Brushes.LightGray, 25),
+            60,
+            1200,
+            false,
+            75
+        );
+
+        CreatePlanetWithMoons(
+            new Planet(20, 20, -0.02, new Vector2D(center.X, center.Y), Brushes.Green, 300),
+            new Moon(2, 2, 0.01, null, Brushes.LightGray, 25),
+            60,
+            1200,
+            true,
+            75
+        );
+
+        CreatePlanetWithMoons(
+            new Planet(60, 60, -0.01, new Vector2D(center.X, center.Y), Brushes.Green, 460),
+            new Moon(8, 8, 0.01, null, Brushes.LightGray, 60),
+            60,
+            8,
+            true,
             40
         );
-        mainCanvas.Children.Add(earthMoon.Shape);
 
-        Planet applePlanet = new(
-            20,
-            20,
-            0.02,
-            new Vector2D(center.X, center.Y),
-            Brushes.Red,
-            300
-        );
-        mainCanvas.Children.Add(applePlanet.Shape);
-        planets.Add(applePlanet);
-
-        Random rand = new Random();
-        for (int i = 0; i < 1200; i++)
-        {
-            Moon randomMoon = new(
-                2,
-                2,
-                0.01 + (rand.NextDouble() / 60),
-                applePlanet,
-                Brushes.LightGray,
-                rand.Next(25, 100)
-            );
-            mainCanvas.Children.Add(randomMoon.Shape);
-            timer.Tick += randomMoon.Move;
-        }
-
-        Planet bigOne = new(
+        CreatePlanetWithMoons(
+            new Planet(60, 60, 0.01, new Vector2D(center.X, center.Y), Brushes.Green, 460),
+            new Moon(8, 8, 0.01, null, Brushes.LightGray, 60),
             60,
-            60,
-            0.01,
-            new Vector2D(center.X, center.Y),
-            Brushes.Green,
-            460
+            8,
+            false,
+            40
         );
-        planets.Add(bigOne);
-
-        for (int i = 0; i < 8; i++)
-        {
-            Moon randomMoon = new(
-                8,
-                8,
-                0.01 + (rand.NextDouble() / 60),
-                bigOne,
-                Brushes.LightGray,
-                rand.Next(60, 100)
-            );
-            mainCanvas.Children.Add(randomMoon.Shape);
-            timer.Tick += randomMoon.Move;
-        }
-        mainCanvas.Children.Add(bigOne.Shape);
-
-        timer.Tick += earth.Move;
-        timer.Tick += sun.Move;
-        timer.Tick += applePlanet.Move;
-        timer.Tick += earthMoon.Move;
-        timer.Tick += bigOne.Move;
 
         timer.Start();
     }
@@ -113,5 +84,33 @@ public partial class MainWindow : Window
     {
         center = new Vector2D(e.NewSize.Width / 2, e.NewSize.Height / 2);
         planets.ForEach((e) => e.SetPosition(new Vector2D(center.X - (e.Shape.Width / 2), center.Y - (e.Shape.Height / 2))));
+    }
+
+    public void CreatePlanetWithMoons(Planet planet, Moon moonBluePrint, double randomSpeedOfMoons, int numberOfMoons, bool moonDirection, int randomnessOfDistance = 0)
+    {
+        planets.Add(planet);
+
+        Random rand = new();
+        int direction = 1;
+
+        if (moonDirection)
+        {
+            direction = -1;
+        }
+
+        for (int i = 0; i < numberOfMoons; i++)
+        {
+            var newMoon = new Moon(moonBluePrint);
+            newMoon.Speed = moonBluePrint.Speed + (rand.NextDouble() / randomSpeedOfMoons) * direction;
+            newMoon.planet = planet;
+            if (randomnessOfDistance > 0)
+            {
+                newMoon.distance += rand.Next(randomnessOfDistance);
+            }
+            mainCanvas.Children.Add(newMoon.Shape);
+            timer.Tick += newMoon.Move;
+        }
+        mainCanvas.Children.Add(planet.Shape);
+        timer.Tick += planet.Move;
     }
 }
